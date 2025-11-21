45f989ae58b387660ebd71357499c1278f54cd58
AlunoWander <gabrieltaveiros.novaroma@gmail.com>
2025-11-21 13:44:06 +0000

    Atualização

--- backend/app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
-const bodyParser = require('body-parser');
-const authRoutes = require('./routes/auth.routes');
-const clientsRoutes = require('./routes/clients.routes');
-const productsRoutes = require('./routes/products.routes');
-const ordersRoutes = require('./routes/orders.routes');
-const { swa
